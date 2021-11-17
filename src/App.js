f2d3b7b307de24a4d9e0e2d1afe4f763179857d9
righetdd <david.righetto@bluewin.ch>
2021-11-17 12:58:25 +0100

    #4 React List Table

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import { DefaultLayout } from "./layout/DefaultLayout";
+// import { Dashboard } from "./pages/dashoard/Dashboard.page";
+
+// import { Entry } from "./pages/entry/Entry.page";
+import { AddTicket } from "./pages/new-ticket/AddTicket.page";
+import { TicketLists } from "./pages/ticket-list/TicketLists.page";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Entry /> */}
+      <DefaultLayout>
+        {/* <Dashboard /> */}
+        {/* <AddTicket /> */}
+        <TicketLists />
+      </DefaultLayout>
+    </div>
+  );
+}
+
+export default App;
